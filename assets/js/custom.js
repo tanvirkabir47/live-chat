622795b107b2c4a5d69b854dad888a4b8d98cfa2
Tanvir Kabir <tanvirkabir47@gmail.com>
2023-09-26 11:37:34 +0600

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,15 @@ $(function() {
   })
   
 })
+
+
+const startChat = document.getElementById("start-chat");
+const userDetailsBox = document.getElementById("user-details-box");
+const chatBox = document.getElementById("chat-box");
+const messageBox = document.getElementById("message-input");
+
+startChat.addEventListener('click', function(){
+  userDetailsBox.classList.add ("d-none");
+  chatBox.classList.remove ("d-none");
+  messageBox.classList.remove ("d-none");
+});
